084cbe8254b90c33cc6b5d52160df78b49f7dbaa
Enric <enricsayos77@gmail.com>
2016-02-10 21:14:36 +0100

    index and login start

--- projecte_E_CarlosPineda_EnricSayos/projecte_E_CarlosPineda_EnricSayos/index.aspx.vb
+++ projecte_E_CarlosPineda_EnricSayos/projecte_E_CarlosPineda_EnricSayos/index.aspx.vb
@@ -2,6 +2,8 @@
     Inherits System.Web.UI.Page
 
     Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+        Dim enterprise As New classEnterprise(1, "Enterprise Of da Boss", "343234243K")
+        Response.Redirect("login.aspx")
 
     End Sub
 
