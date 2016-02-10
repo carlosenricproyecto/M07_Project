563ad625e61379f1e538aa12304c8f7fad5b2862
Enric <enricsayos77@gmail.com>
2016-02-10 21:18:16 +0100

    dsf

--- projecte_E_CarlosPineda_EnricSayos/projecte_E_CarlosPineda_EnricSayos/index.aspx.vb
+++ projecte_E_CarlosPineda_EnricSayos/projecte_E_CarlosPineda_EnricSayos/index.aspx.vb
@@ -2,7 +2,14 @@
     Inherits System.Web.UI.Page
 
     Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
-        Dim enterprise As New classEnterprise(1, "Enterprise Of da Boss", "343234243K")
+        If Session("login") Is Nothing Then
+
+            Dim enterprise As New classEnterprise(1, "Enterprise Of da Boss", "343234243K")
+
+        Else
+
+        End If
+
         Response.Redirect("login.aspx")
 
     End Sub
