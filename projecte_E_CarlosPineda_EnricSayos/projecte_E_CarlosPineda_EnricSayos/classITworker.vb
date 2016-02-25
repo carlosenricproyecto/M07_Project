2b9ea2d361930fb7b034891b4bd9d8888b09299e
Enric <enricsayos77@gmail.com>
2016-02-25 21:01:04 +0100

    Commit categories

--- projecte_E_CarlosPineda_EnricSayos/projecte_E_CarlosPineda_EnricSayos/classITworker.vb
+++ projecte_E_CarlosPineda_EnricSayos/projecte_E_CarlosPineda_EnricSayos/classITworker.vb
@@ -2,7 +2,7 @@
     Inherits classWorker
     Protected Shadows Const hourlyWage = 80
     Public Sub New(workerId As Integer, name As String, surname1 As String, surname2 As String, dni As String)
-        MyBase.New(workerId, name, surname1, surname2, dni)
+        MyBase.New(workerId, name, surname1, surname2, dni, New List(Of String) From {"hardware", "software"})
     End Sub
 
 End Class
