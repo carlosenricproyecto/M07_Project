65f44dde86e86cdf2368d485e6536095d3f3717f
Enric <enricsayos77@gmail.com>
2016-03-07 14:09:58 +0100

    basurilla

--- projecte_E_CarlosPineda_EnricSayos/projecte_E_CarlosPineda_EnricSayos/newWorker.aspx.vb
+++ projecte_E_CarlosPineda_EnricSayos/projecte_E_CarlosPineda_EnricSayos/newWorker.aspx.vb
@@ -3,6 +3,9 @@
 
     Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
         If Not Page.IsPostBack Then
+            For i = 0 To workerTypes.Count
+                DropDownList1.Items.Add(workerTypes.Item(i))
+            Next
 
         End If
     End Sub
